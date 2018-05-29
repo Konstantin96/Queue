797096adebb3d597dac4e0a264f52e0f1954ce3c
Клюкинк <@227_11>
2018-05-29 11:43:34 +0600

    123

--- Queue/Source.cpp
+++ Queue/Source.cpp
@@ -4,55 +4,65 @@
 int main()
 {
 	int fg;
-	cout << "List(1) or Queue(2) : ";
-	cin >> fg;
-	switch (fg)
-	{
-	case 1:
-	{
-		NodeQueue<int> lis;
-		lis.enqueue(10);
-		lis.enqueue(20);
-		lis.enqueue(30);
-		lis.enqueue(40);
-		cout << "Elements do: ";
-		lis.print();
-		lis.dequeue();
-		lis.dequeue();
-		cout << "Elements posle: ";
-		lis.print();
-	}break;
-	case 2:
-	{
-		cout << "Enter number chisel: ";
-		int count;
-		cin >> count;
-		Queue<char> qu(count);
-		int i = 0;
-		char c;
-		while (i++ < count)
+	do {
+		system("cls");
+		cout << "List(1) or Queue(2) : ";
+		cin >> fg;
+	
+	
+		switch (fg)
 		{
-			cin >> c;
-			qu.enqueue(c);
+		case 1:
+		{
+			NodeQueue<int> lis;
+			lis.enqueue(10);
+			lis.enqueue(20);
+			lis.enqueue(30);
+			lis.enqueue(40);
+			cout << "Elements do: ";
+			lis.print();
+			lis.dequeue();
+			lis.dequeue();
+			cout << "Elements posle: ";
+			lis.print();
+			system("pause");
+		}break;
+		case 2:
+		{
+			cout << "Enter number chisel: ";
+			int count;
+			cin >> count;
+			Queue<char> qu(count);
+			int i = 0;
+			char c;
+			while (i++ < count)
+			{
+				cin >> c;
+				qu.enqueue(c);
+			}
+
+			cout << "Do : ";
+			qu.print();
+
+			//delete stack
+			qu.dequeue();
+			qu.dequeue();
+			qu.dequeue();
+
+			cout << "Later : ";
+			qu.print();
+			system("pause");
+		}break;
+		default:
+		{
+			cout << "Number error!\n";
+			system("pause");
+		}	break;
+	
 		}
-
-		cout << "Do : ";
-		qu.print();
-
-		//delete stack
-		qu.dequeue();
-		qu.dequeue();
-		qu.dequeue();
-
-		cout << "Later : ";
-		qu.print();
-	}break;
-	default:
-	{cout << "Number error!";
-	}	break;
-	}
-
-
+	} while (fg != 999);
+	
 	system("pause");
+
 	return 0;
 }
